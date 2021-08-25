5b711c6c5de434c3abb5f7c0d3eee57742a00239
Raphael de Carvalho <65046498+RaphaelDeCarvalho@users.noreply.github.com>
2021-08-25 17:29:40 -0300

    script atualizado
    alterado a fonte de imagens para a melhot visualização do código

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
 if(hora >= 0 && hora < 12){
     //Bom dia! #E9542C
-    img.src = 'img/manha.png'
+    img.src = 'manha.png'
     document.body.style.background = '#E9542C'
     comenta.innerHTML = `<strong>Bom Dia!</strong>`
 } else if (hora >= 12 && hora <= 18){
     //Boa tarde! #91B19D
-    img.src = 'img/tarde.png'
+    img.src = 'tarde.png'
     document.body.style.background = '#DDAA7F'
     comenta.innerHTML = `<strong>Boa Tarde!</strong>`
 } else {
     //Boa noite! #454545
-    img.src = 'img/noite.png'
+    img.src = 'noite.png'
     document.body.style.background = '#454545'
     comenta.innerHTML = `<strong>Boa Noite!</strong>`
 }
